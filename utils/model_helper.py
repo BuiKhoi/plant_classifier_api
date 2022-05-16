bdddd3d3e53fffb2a9238674e11fcd91649b43bc
BuiKhoi <khoibui2319@gmail.com>
2022-05-16 23:53:43 +0700

    Add center crop for predict function

--- utils/model_helper.py
+++ utils/model_helper.py
@@ -17,8 +17,22 @@ inferer = face_learner(conf, const.strange_threshold)
 inferer.load_state(const.model_path)
 
 
+def center_crop_image(im):
+    h, w, _ = im.shape
+    smaller_size = min(w, h)
+    new_size = int(smaller_size*0.8)
+
+    width_start = int((w-new_size)/2)
+    width_stop = (width_start + new_size)
+
+    height_start = int((h-new_size)/2)
+    height_stop = height_start + new_size
+
+    return im[height_start:height_stop, width_start:width_stop, :]
+
+
 def predict_image(image):
     if image.shape[:2] != const.predict_size:
-        image = cv2.resize(image, const.predict_size)
+        image = cv2.resize(center_crop_image(image), const.predict_size)
 
     return inferer.infer(conf, Image.fromarray(image[:, :, ::-1]), embeddings, True, labels)
